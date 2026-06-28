0c451f6acba9cea31296b5620adc353ff93339f8
“Jennie <hello@treeduckdesign.com>
2026-06-28 12:17:24 +0100

    adding ReadmeGenerator class

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ from template import generate_template
 from writer import save_readme
 
 
+class ReadmeGenerator:
+    def __init__():
+        self.answers = None
+        self.contact_details = None
+        self.readme_content = None
 
+    def run(self):
+        self.answers = get_answers()
+        self.contact_details = get_contact_details()
+        validate_contact_details(self.contact_details)
 
-answers = get_answers()
-contact_details = get_contact_details()
-validate_contact_details(contact_details)
+        self.readme_content = generate_template(self.answers, self.contact_details)
+        save_readme(self.readme_content)
 
-readme_content = generate_template(answers, contact_details)
-save_readme(readme_content)
+        print(self.readme_content)
 
-print(readme_content)
+generator = ReadmeGenerator()
+generator.run()
